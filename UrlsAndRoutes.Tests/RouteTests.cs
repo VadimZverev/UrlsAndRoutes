abf3baeb4115d9057d867b5823cab1978f291869
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-08-21 13:26:58 +0400

    Юнит тест: тестирование входящих URL

---
+++ UrlsAndRoutes.Tests/RouteTests.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Reflection;
+using System.Web;
+using System.Web.Routing;
+
+
+namespace UrlsAndRoutes.Tests
+{
+    [TestClass]
+    public class RouteTests
+    {
+        private HttpContextBase CreateHttpContext(string targetUrl = null, string httpMethod = "GET")
+        {
+            // создаём mock-запрос
+            Mock<HttpRequestBase> mockRequest = new Mock<HttpRequestBase>();
+            mockRequest.Setup(m => m.AppRelativeCurrentExecutionFilePath)
+                .Returns(targetUrl);
+            mockRequest.Setup(m => m.HttpMethod).Returns(httpMethod);
+
+            // создаём mock-ответ
+            Mock<HttpResponseBase> mockResponse = new Mock<HttpResponseBase>();
+            mockResponse.Setup(m => m.ApplyAppPathModifier(It.IsAny<string>()))
+                .Returns<string>(s => s);
+
+            // Создаём mock-контекст, используя запрос и ответ
+            Mock<HttpContextBase> mockContext = new Mock<HttpContextBase>();
+            mockContext.Setup(m => m.Request).Returns(mockRequest.Object);
+            mockContext.Setup(m => m.Response).Returns(mockResponse.Object);
+
+            // возвращаем mock-контекст
+            return mockContext.Object;
+        }
+
+        private bool TestIncomingRouteResult(RouteData routeResult, string controller, string action, object propertySet = null)
+        {
+            Func<object, object, bool> valCompare = (v1, v2) =>
+            {
+                return StringComparer.InvariantCultureIgnoreCase
+                .Compare(v1, v2) == 0;
+            };
+
+            bool result = valCompare(routeResult.Values["controller"], controller)
+                && valCompare(routeResult.Values["action"], action);
+
+            if (propertySet != null)
+            {
+                PropertyInfo[] propInfo = propertySet.GetType().GetProperties();
+                foreach (PropertyInfo pi in propInfo)
+                {
+                    if (!(routeResult.Values.ContainsKey(pi.Name)
+                        && valCompare(routeResult.Values[pi.Name],
+                        pi.GetValue(propertySet, null))))
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private void TestRouteMatch(string url, string controller, string action, object routeProperties = null, string httpMethod = "GET")
+        {
+            // Arrange
+            RouteCollection routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+
+            // Act - обрабатываем роут(маршрут)
+            RouteData result
+                = routes.GetRouteData(CreateHttpContext(url, httpMethod));
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(TestIncomingRouteResult(result, controller,
+                action, routeProperties));
+        }
+
+        private void TestRouteFail(string url)
+        {
+            // Arrange
+            RouteCollection routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+
+            // Act - обработка роута(маршрута)
+            RouteData result = routes.GetRouteData(CreateHttpContext(url));
+
+            // Assert
+            Assert.IsTrue(result == null || result.Route == null);
+        }
+
+        [TestMethod]
+        public void TestIncomingRoutes()
+        {
+            // проверяем на URL, который мы хотим получить
+            TestRouteMatch("~/Admin/Index", "Admin", "Index");
+
+            // Проверка на то, что значения были получены из сегментов
+            TestRouteMatch("~/One/Two", "One", "Two");
+
+            // гарантия того, что слишком много или слишком мало сегментов не походят
+            TestRouteFail("~/Admin/Index/Segment");
+            TestRouteFail("~/Admin");
+        }
+    }
+}
