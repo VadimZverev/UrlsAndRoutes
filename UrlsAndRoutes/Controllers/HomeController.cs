5d9dc86cbfac7be9027da3c488755f52b2183077
Вадим Зверев <WadimZverev@gmail.com>
2018-08-22 00:20:12 +0400

    Использование пользовательских переменных в качестве параметра метода действия

--- UrlsAndRoutes/Controllers/HomeController.cs
+++ UrlsAndRoutes/Controllers/HomeController.cs
@@ -15,12 +15,12 @@ namespace UrlsAndRoutes.Controllers
             return View("ActionName");
         }
 
-        public ActionResult CustomVariable()
+        public ActionResult CustomVariable(string id)
         {
             ViewBag.Controller = "Home";
             ViewBag.Action = "CustomVariable";
             // Доступ к пользовательской переменной сегмента в методе действия
-            ViewBag.CustomVariable = RouteData.Values["id"];
+            ViewBag.CustomVariable = id;
             return View();
         }
     }
