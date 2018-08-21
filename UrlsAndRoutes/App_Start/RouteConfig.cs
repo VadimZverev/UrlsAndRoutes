c989fb74755aed82337ab9d50d5619db23c8e67c
Вадим Зверев <WadimZverev@gmail.com>
2018-08-21 22:58:16 +0400

    URL паттерн со статическими сегментами

--- UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -10,7 +10,10 @@ namespace UrlsAndRoutes
     public class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
-        {
+        {   
+            routes.MapRoute("", "Public/{controller}/{action}",
+                new { controller = "Home", action = "Index" });
+
             routes.MapRoute("MyRoute", "{controller}/{action}",
                 new { controller = "Home", action = "Index" });
         }
