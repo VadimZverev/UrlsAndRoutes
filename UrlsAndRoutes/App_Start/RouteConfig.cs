9a35fa37eb28f48125f09dc5c294a394efae232f
Вадим Зверев <WadimZverev@gmail.com>
2018-08-21 23:52:49 +0400

    Определение дополнительных переменных в URL паттерне

--- UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -11,24 +11,13 @@ namespace UrlsAndRoutes
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            //  Создание алиасов для контроллера и действия
-            routes.MapRoute("ShopSchema2", "Shop/OldAction",
-                new { controller = "Home", action = "index" });
-
-            //  Смешивание статических URL сегментов и значений по умолчанию
-            routes.MapRoute("ShopSchema", "Shop/{action}",
-                new { controller = "Home" });
-
-            // URL паттерн со смешанным сегментом
-            routes.MapRoute("", "X{controller}/{action}");
-
-            // роут значений по умолчанию для действия и контроллера
             routes.MapRoute("MyRoute", "{controller}/{action}",
-                new { controller = "Home", action = "Index" });
-
-            // URL паттерн со статическими сегментами
-            routes.MapRoute("", "Public/{controller}/{action}",
-                new { controller = "Home", action = "Index" });
+                new
+                {
+                    controller = "Home",
+                    action = "Index",
+                    id = "DefaultId"
+                });
         }
     }
 }
