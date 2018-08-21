c978219470888904e63651943c5fc1d48f24e16d
Вадим Зверев <WadimZverev@gmail.com>
2018-08-21 23:54:51 +0400

    правки

--- UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -11,6 +11,7 @@ namespace UrlsAndRoutes
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            // Определение дополнительных переменных в URL паттерне
             routes.MapRoute("MyRoute", "{controller}/{action}/{id}",
                 new
                 {
