fd80955f1fb54c89d9930a93225dbf3884fbe422
Вадим Зверев <WadimZverev@gmail.com>
2018-08-22 21:31:39 +0400

    Генерирование URL из конкретного роута

--- UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -12,16 +12,8 @@ namespace UrlsAndRoutes
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            //routes.MapRoute("NewRoute", "App/Do{action}",
-            //    new { controller = "Home" });
-
-            routes.MapRoute("MyRoute", "{controller}/{action}/{id}",
-                new
-                {
-                    controller = "Home",
-                    action = "Index",
-                    id = UrlParameter.Optional
-                });
+            routes.MapRoute("MyRoute", "{controller}/{action}");
+            routes.MapRoute("MyOtherRoute", "App/{action}", new { controller = "Home" });
         }
     }
 }
