5047b0223dbe0e525f3fcefd1cad10b08c640bae
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-08-22 12:38:24 +0400

    Определение пользовательского ограничения
    - Создание пользовательского ограничения роутов UserAgentConstraint ;
    - Применение пользовательского ограничения к роуту

--- UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using UrlsAndRoutes.Infrastructure;
 
 namespace UrlsAndRoutes
 {
@@ -11,6 +12,14 @@ namespace UrlsAndRoutes
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.MapRoute("ChromeRoute", "{*catchall}",
+                new { controller = "Home", action = "Index" },
+                new
+                {
+                    customConstraint = new UserAgentConstraint("Chrome")
+                },
+                new[] { "UrlsAndRoutes.AdditionalControllers" });
+
             routes.MapRoute("MyRoute", "{controller}/{action}/{id}/{*catchall}",
                 new { controller = "Home", action = "Index", id = UrlParameter.Optional },
                 new
